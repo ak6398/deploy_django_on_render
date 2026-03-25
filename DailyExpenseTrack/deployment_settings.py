39f69bdcc9d23523ef87e24681f9183f727ea685
ankit <as9771184@gmail.com>
2026-03-25 12:57:56 +0530

    addurl

--- DailyExpenseTrack/deployment_settings.py
+++ DailyExpenseTrack/deployment_settings.py
@@ -34,6 +34,6 @@ STORAGES={
 }
 
 DATABASES={
-    "default":dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=600),
+    "default":dj_database_url.config(default=os.environ.get['DATABASE_URL'],conn_max_age=600),
     
 }
